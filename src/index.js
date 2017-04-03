eabd595c793824505e8b9956768b6a42b72dc881
Mel Rabineau <melanie.rabineau1@gmail.com>
2017-04-02 19:37:36 -0700

    fixing bugs for heroku deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import AllPlants from './components/AllPlants.js'
-import SinglePlant from './components/SinglePlant.js'
 import PlantComponent from './components/PlantComponent.js'
 import './index.css';
 import { Router, Route, hashHistory } from 'react-router'
@@ -13,7 +12,6 @@ ReactDOM.render((
   <Router history={hashHistory}>
     <Route path='/' component={App} />
     <Route path='/allplants' component={AllPlants} />
-    <Route path='/singleplant' component={SinglePlant} />
     <Route path='/plants' component={PlantComponent} />
   </Router>
 ),document.getElementById('root'));
